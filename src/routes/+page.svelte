856b137071698b4acaa6bc7c13b9eb59242aeaf9
joel <91632635+joeliscoding@users.noreply.github.com>
2026-01-01 12:37:39 +0100

    improving notes array, starting mediastream implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,57 @@
 <script>
+    /*
+        & Treble Clef
+        ¯ Bass Clef
+    */
+
     import { onMount } from 'svelte';
     import { on } from 'svelte/events';
 
-    let note_index = 0;
+    let recorder = null;
+
+    // onMount(() => {
+    //     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    //         navigator.mediaDevices
+    //             .getUserMedia({ audio: true })
+    //             .then(SetupStream)
+    //             .catch((err) => {
+    //                 console.error(err);
+    //             });
+    //     }
+    // });
+
+    /**
+     * @param {MediaStream} stream
+     */
+    function SetupStream(stream) {
+        recorder = new MediaRecorder(stream);
+    }
+
+    let noteIndex = 0;
     let display = '';
-    const display_notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'];
     const notes = [
-        'B3',
-        'C4',
-        'D4',
-        'E4',
-        'F4',
-        'G4',
-        'A4',
-        'B4',
-        'C5',
-        'D5',
-        'E5',
-        'F5',
-        'G5',
-        'A5'
+        { trebleName: 'A3', bassName: 'C2', whole: 'p', half: '`', quarter: 'P' },
+        { trebleName: 'B3', bassName: 'D2', whole: 'q', half: 'a', quarter: 'Q' },
+        { trebleName: 'C4', bassName: 'E2', whole: 'r', half: 'b', quarter: 'R' },
+        { trebleName: 'D4', bassName: 'F2', whole: 's', half: 'c', quarter: 'S' },
+        { trebleName: 'E4', bassName: 'G2', whole: 't', half: 'd', quarter: 'T' },
+        { trebleName: 'F4', bassName: 'A2', whole: 'u', half: 'e', quarter: 'U' },
+        { trebleName: 'G4', bassName: 'B2', whole: 'v', half: 'f', quarter: 'V' },
+        { trebleName: 'A4', bassName: 'C3', whole: 'w', half: 'g', quarter: 'W' },
+        { trebleName: 'B4', bassName: 'D3', whole: 'x', half: 'h', quarter: 'X' },
+        { trebleName: 'C5', bassName: 'E3', whole: 'y', half: 'i', quarter: 'Y' },
+        { trebleName: 'D5', bassName: 'F3', whole: 'z', half: 'j', quarter: 'Z' },
+        { trebleName: 'E5', bassName: 'G3', whole: '{', half: 'k', quarter: '[' },
+        { trebleName: 'F5', bassName: 'A3', whole: '|', half: 'l', quarter: '\\' },
+        { trebleName: 'G5', bassName: 'B3', whole: '}', half: 'm', quarter: ']' },
+        { trebleName: 'A5', bassName: 'C4', whole: '~', half: 'n', quarter: '^' }
     ];
-    const changeNote = () => {
-        note_index = Math.round(Math.random() * (display_notes.length - 1));
-        display = `'&=${display_notes[note_index]}!`;
-    };
+
+    function changeNote() {
+        noteIndex = Math.round(Math.random() * (notes.length - 1));
+        display = `'&=${notes[noteIndex].whole}!`;
+    }
+
     onMount(() => {
         changeNote();
         const interval = setInterval(changeNote, 2000);
@@ -33,7 +60,7 @@
 </script>
 
 <div>
-    <p class="sheet-music" id="display">{display}</p>
+    <p class="sheet-music" id="display"></p>
 </div>
 
 <style lang="scss">
